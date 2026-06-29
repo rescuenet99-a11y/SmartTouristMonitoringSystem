5f49969539758b3ae57b8323b6299fb0fd058071
Valli-Chandrasekar <vallichandrasekar5@gmail.com>
2026-06-29 14:30:35 +0530

    Initial commit

--- app.py
+++ app.py
@@ -1,10 +1,3 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
-
-@app.route("/")
-def tracking():
-    return render_template("tracking.html")
-
-if __name__ == "__main__":
-    app.run(debug=True)
